ae4660f9678a94edda1a58854452d76634d842c3
er-vikas-mishra <albert.mishra@rediffmail.com>
2023-05-21 05:09:40 +0530

    CSS Formatting updated

--- javascript/clock.js
+++ javascript/clock.js
@@ -34,16 +34,16 @@ window.onload = function () {
     }, 1000);
 
     let anthology = {
-        0: "जहाँ रहेगा वहीं रौशनी लुटाएगा <br>किसी चराग़ का अपना मकाँ नहीं होता<br><br>~-~-~ Waseem Barelvi ~-~-~",
-        1: "बड़े लोगों से मिलने में हमेशा फ़ासला रखना <br>जहाँ दरिया समुंदर से मिला दरिया नहीं रहता<br><br>~-~-~ Basheer Badr ~-~-~",
-        2: "वो बात सारे फ़साने में जिस का ज़िक्र न था <br>वो बात उन को बहुत ना-गवार गुज़री है<br><br>~-~-~ Faiz Ahmad Faiz ~-~-~",
-        3: "कोई हाथ भी न मिलाएगा जो गले मिलोगे तपाक से <br>ये नए मिज़ाज का शहर है ज़रा फ़ासले से मिला करो<br><br>~-~-~ Basheer Badr ~-~-~",
-        4: "सुब्ह होती है शाम होती है <br>उम्र यूँही तमाम होती है<br><br>~-~-~ Munshi Amirullah Tasleem ~-~-~",
-        5: "घर से मस्जिद है बहुत दूर चलो यूँ कर लें <br>किसी रोते हुए बच्चे को हँसाया जाए<br><br>~-~-~ Nida Fazli ~-~-~",
-        6: "यहाँ लिबास की क़ीमत है आदमी की नहीं <br>मुझे गिलास बड़े दे शराब कम कर दे<br><br>~-~-~ Basheer Badr ~-~-~",
-        7: "इशरत-ए-क़तरा है दरिया में फ़ना हो जाना <br>दर्द का हद से गुज़रना है दवा हो जाना <br><br>~-~-~ Mirza Ghalib ~-~-~",
-        8: "दिल ना-उमीद तो नहीं नाकाम ही तो है <b>लम्बी है ग़म की शाम मगर शाम ही तो है <br><br>~-~-~ Faiz Ahmad Faiz ~-~-~",
-        9: "किसी को घर मिला हिस्से में या कोई दुकाँ आई <br>मैं घर में सब से छोटा था मिरे हिस्से में माँ आई <br><br>~-~-~ Munawwar Rana ~-~-~"
+        0: "<h3><p>जहाँ रहेगा वहीं रौशनी लुटाएगा</p>किसी चराग़ का अपना मकाँ नहीं होता।<br><br><footer class='blockquote-footer'>Waseem Barelvi</footer></h3>",
+        1: "<h3><p>बड़े लोगों से मिलने में हमेशा फ़ासला रखना</p>जहाँ दरिया समुंदर से मिला दरिया नहीं रहता।<br><br><footer class='blockquote-footer'>Basheer Badr</footer></h3>",
+        2: "<h3><p>वो बात सारे फ़साने में जिस का ज़िक्र न था</p>वो बात उन को बहुत ना-गवार गुज़री है।<br><br><footer class='blockquote-footer'>Faiz Ahmad Faiz</footer></h3>",
+        3: "<h3><p>कोई हाथ भी न मिलाएगा जो गले मिलोगे तपाक से</p>ये नए मिज़ाज का शहर है ज़रा फ़ासले से मिला करो।<br><br><footer class='blockquote-footer'>Basheer Badr</footer></h3>",
+        4: "<h3><p>सुब्ह होती है शाम होती है</p>उम्र यूँही तमाम होती है।<br><br><footer class='blockquote-footer'>Munshi Amirullah Tasleem</footer></h3>",
+        5: "<h3><p>घर से मस्जिद है बहुत दूर चलो यूँ कर लें</p>किसी रोते हुए बच्चे को हँसाया जाए।<br><br><footer class='blockquote-footer'>Nida Fazli</footer></h3>",
+        6: "<h3><p>यहाँ लिबास की क़ीमत है आदमी की नहीं</p>मुझे गिलास बड़े दे शराब कम कर दे।<br><br><footer class='blockquote-footer'>Basheer Badr</footer></h3>",
+        7: "<h3><p>इशरत-ए-क़तरा है दरिया में फ़ना हो जाना</p>दर्द का हद से गुज़रना है दवा हो जाना।<br><br><footer class='blockquote-footer'>Mirza Ghalib</footer></h3>",
+        8: "<h3><p>दिल ना-उमीद तो नहीं नाकाम ही तो है</p>लम्बी है ग़म की शाम मगर शाम ही तो है।<br><br><footer class='blockquote-footer'>Faiz Ahmad Faiz</footer></h3>",
+        9: "<h3><p>किसी को घर मिला हिस्से में या कोई दुकाँ आई</p>मैं घर में सब से छोटा था मिरे हिस्से में माँ आई।<br><br><footer class='blockquote-footer'>Munawwar Rana</footer></h3>"
     }
 
     sher = anthology[parseInt(Math.random() * 10)];
